f298d9780bb2e9f34f46a605d9eeb7b94d517b2e
shasherazi <hassanrandomz@gmail.com>
2026-06-10 17:40:11 +0500

    fresh start

--- src/modules/redditSender/unixporn.js
+++
@@ -1,8 +0,0 @@
-const getImgUrl = async () => {
-    const res = await fetch('https://meme-api.com/gimme/unixporn');
-    const data = await res.json();
-    const result = { url: data.url, title: data.title, postLink: data.postLink };
-    return result;
-};
-
-export { getImgUrl };
